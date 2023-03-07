b474e1a1040fb44c18a0eb76c070763c839e484f
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-03-07 15:08:43 +0100

    ajustement des marge

--- src/components/Carte.jsx
+++ src/components/Carte.jsx
@@ -2,13 +2,13 @@ import React from "react"
 import "../styles/Carte.css"
 import {Link} from 'react-router-dom'
 
-
+//Fonction qui gère le modèle de carte
 function Carte({image, titre, id}){
 
     return(
-        
+        //La balise de lien englobe toutes la cartes, et lors du clique renvois l'id du logement dans l'url
         <div >
-            <Link to={`/Logement?${id}`} className="full-carte">
+            <Link to={`/Logement?${id}`} className="full-carte"> 
                 <img className="image-carte" src={image} alt="" />
                 <div className="shadow-carte"></div>
                 <h2 className="titre-carte">{titre}</h2>
@@ -18,3 +18,4 @@ function Carte({image, titre, id}){
 }
 
 export default Carte
+//renvois vers Logement.jsx
